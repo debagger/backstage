e600aa129d0763e84d9725590f903b4e61f5335b
debagger <debagger@gmail.com>
2020-12-28 11:18:54 +0500

    Fix no Bearer prefix in axios auth header

--- frontend/middleware/set-auth-token.ts
+++ frontend/middleware/set-auth-token.ts
@@ -7,7 +7,7 @@ const myMiddleware: Middleware = async ({ app }) => {
     const token = await auth.getTokenSilently()
     console.log('[apollo-auth] token = ', token)
     app.$apolloHelpers.onLogin(token)
-    app.$axios.setToken(token)
+    app.$axios.setToken(token, "Bearer")
   } else {
     console.log('[apollo-auth] not authenticated')
     app.$apolloHelpers.onLogout()
--- frontend/nuxt.config.js
+++ frontend/nuxt.config.js
@@ -23,7 +23,7 @@ export default {
   css: [],
 
   // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
-  plugins: ['~/plugins/auth.client.ts', '~/plugins/axios.ts'],
+  plugins: ['~/plugins/auth.client.ts'],
   // Auto import components (https://go.nuxtjs.dev/config-components)
   components: true,
 
